c3e356667c863eee07acbc6a6de025dd61fe0d55
shivam kumar <contactshivam355@gmail.com>
2018-01-28 17:38:04 +0530

    added website

--- angular/src/app/app-routing-module.ts
+++ angular/src/app/app-routing-module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from 'app/component/page-not-found/page-not-found.component';
-import { WebsiteComponent } from 'app/component/website/website.component';
 import { LoginComponent } from 'app/component/login/login.component';
 import { HomeComponent } from 'app/component/home/home.component';
+import { ContactusComponent } from 'app/component/contactus/contactus.component';
+import { BlogComponent } from 'app/component/blog/blog.component';
+import { BlogPostComponent } from 'app/component/blog-post/blog-post.component';
 
 
 const routes: Routes = [
-  { path: '', component: WebsiteComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'blogpost', component: BlogPostComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,20 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
-import { WebsiteComponent } from './component/website/website.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 import { AppRoutingModule } from 'app/app-routing-module';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ContactusComponent } from './component/contactus/contactus.component';
+import { BlogComponent } from './component/blog/blog.component';
+import { BlogPostComponent } from './component/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WebsiteComponent,
     PageNotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactusComponent,
+    BlogComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
