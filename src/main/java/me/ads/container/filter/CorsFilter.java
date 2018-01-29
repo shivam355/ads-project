af97600d0654645956865b865fb5f0d6b33a0dd6
shivam kumar <contactshivam355@gmail.com>
2018-01-29 23:45:24 +0530

    added config for hosting

--- src/main/java/me/ads/container/filter/CorsFilter.java
+++ src/main/java/me/ads/container/filter/CorsFilter.java
@@ -11,34 +11,34 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+	private final Logger log = LoggerFactory.getLogger(CorsFilter.class);
+
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// Intentionally empty.
+		log.info("CorsFilter init");
 	}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
+		log.error("doFilter called");
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With");
 
-		// Returns an empty OK response on any OPTION request, otherwise proceed
-		// with the filter chain.
-		if (HttpMethod.OPTIONS.matches(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
-		} else {
-			filterChain.doFilter(request, response);
-		}
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+		filterChain.doFilter(request, response);
 	}
 
 	public void destroy() {
